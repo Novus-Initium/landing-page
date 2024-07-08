bf38013f3376d1c8a9dbf57907adabbe458a4c5f
innkeep <innkeeping@metagame.wtf>
2024-07-08 00:21:26 -0500

    Update astro.config.mjs 🥅
    add ninit.net

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,6 @@ export default defineConfig({
     skipInline: false,
     drafts: true
   },
-  site: 'https://Ninitthemes.com',
+  site: 'https://ninit.net',
   integrations: [tailwind(), sitemap(), mdx(), devtoolBreakpoints()]
 });
